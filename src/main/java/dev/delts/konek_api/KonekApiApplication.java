72adca568fb633bde5fac0ac3f3bc298c0a895f4
Delthoid <delthonvillanueva20@gmail.com>
2025-10-11 07:24:47 +0800

    Initialize

---
+++ src/main/java/dev/delts/konek_api/KonekApiApplication.java
@@ -0,0 +1,13 @@
+package dev.delts.konek_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonekApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/delts/konek_api/KonekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.delts.konek_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
