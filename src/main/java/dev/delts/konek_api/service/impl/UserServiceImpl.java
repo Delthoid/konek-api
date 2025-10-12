36a34c5b22223b33b29d8be5450487871870d800
Delthoid <delthonvillanueva20@gmail.com>
2025-10-12 22:56:20 +0800

    Update UserServiceImpl.java

--- src/main/java/dev/delts/konek_api/service/impl/UserServiceImpl.java
+++ src/main/java/dev/delts/konek_api/service/impl/UserServiceImpl.java
@@ -1,13 +1,12 @@
 package dev.delts.konek_api.service.impl;
 
 import dev.delts.konek_api.dto.UserDtoResponse;
-import dev.delts.konek_api.dto.auth.LoginRequest;
+import dev.delts.konek_api.dto.request.auth.LoginRequest;
 import dev.delts.konek_api.dto.auth.LoginResponse;
-import dev.delts.konek_api.dto.auth.SignUpRequest;
+import dev.delts.konek_api.dto.request.auth.SignUpRequest;
 import dev.delts.konek_api.entity.User;
 import dev.delts.konek_api.exception.RecordAlreadyExistsException;
 import dev.delts.konek_api.repository.UserRepository;
-import dev.delts.konek_api.service.CustomUserDetailsService;
 import dev.delts.konek_api.service.UserService;
 import dev.delts.konek_api.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
